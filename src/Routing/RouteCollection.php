6b95477160d0a391c86a69b3e0647fe80a44850a
Kieron <info@kieronwilson.co.uk>
2016-02-29 11:11:56 +0000

    updates

--- src/Routing/RouteCollection.php
+++ src/Routing/RouteCollection.php
@@ -4,38 +4,71 @@ namespace Routing;
 
 class RouteCollection implements CollectionInterface {
 
-	protected $routes;
+	protected $keys = [];
+
+	protected $values = [];
 
 	public function __construct(array $routes = []) {
 		$this->set($routes);
 	}
 
 	public function getIterator() {
-		return new \ArrayIterator($this->routes);
+		return new \ArrayIterator($this->get());
+	}
+
+	public function append($path, $route) {
+		array_push($this->keys, $path);
+		array_push($this->values, $route);
+
+		return $this;
+	}
+
+	public function appends(array $routes) {
+		foreach($routes as $key => $value) {
+			$this->append($key, $value);
+		}
+
+		return $this;
+	}
+
+	public function prepend($path, $route) {
+		array_unshift($this->keys, $path);
+		array_unshift($this->values, $route);
+
+		return $this;
+	}
+
+	public function prepends(array $routes) {
+		foreach($routes as $key => $value) {
+			$this->prepend($key, $value);
+		}
+
+		return $this;
 	}
 
 	public function get() {
-		return $this->routes;
+		return array_combine($this->keys, $this->values);
 	}
 
 	public function set(array $routes) {
-		$this->routes = $routes;
+		$this->keys = array_keys($routes);
+		$this->values = array_values($routes);
 
 		return $this;
 	}
 
 	public function merge(array $routes) {
-		$this->set(array_merge($this->routes, $routes));
-
-		return $this;
+		return $this->appends($routes);
 	}
 
 	public function has($path) {
-		return array_key_exists($path, $this->routes);
+		return in_array($path, $this->keys, true);
 	}
 
 	public function route($path) {
-		return $this->routes[$path];
+		$index = array_search($path, $this->keys, true);
+
+		return $this->values[$index];
 	}
 
 }
